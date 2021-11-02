bc2851a861a279763e68268bd09f43769bb56b15
Stanisław Kryj <cookieblackxd@gmail.com>
2021-11-02 16:05:35 +0100

    Change slack_url to env

--- main.go
+++ main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"log"
+	"os"
 	"stankryj/WorkEpicProject/epic"
 	"time"
 )
 
 func main() {
-	epicUrl := "https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions?locale=pl&country=PL&allowCountries=PL"
-	slackUrl := "https://hooks.slack.com/services/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"
-
+	epicURL := "https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions?locale=pl&country=PL&allowCountries=PL"
+	slackURL, exists := os.LookupEnv("SLACKURL")
+	if exists == false {
+		log.Fatal("Didn't find environment variable named SLACKURL")
+	}
 	var oldGame string
 
 	for {
-		epic.EpicWeekly(epicUrl, slackUrl, &oldGame)
+		epic.EpicWeekly(epicURL, slackURL, &oldGame)
 		time.Sleep(time.Minute * 60)
 	}
 }
